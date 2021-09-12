d2487b7bc488d49e2820b4cef0535d27ba808d90
DiaAudi <61022208+DiaAudi@users.noreply.github.com>
2021-09-12 17:33:33 +0000

    warnings

--- HighwayPatrol.cpp
+++ HighwayPatrol.cpp
@@ -3,6 +3,11 @@
 #include <cassert>
 #include "HighwayPatrol.h"
 #include "Highway.h"
+#include "Car.h"
+#include "Motorcycle.h"
+#include "SemiTruck.h"
+
+
 HighwayPatrol::HighwayPatrol() : Vehicle("HighwayPatrol")
 {
 
@@ -29,9 +34,23 @@ void HighwayPatrol::pullOver( Vehicle* v, bool willArrest, Highway* h )
     std::cout << name << ": vehicle is traveling " << v->speed - h->speedLimit << " miles per hour over the speed limit" << std::endl;
     if( willArrest )
     {
-        assert(false);
+        //assert(false);
         //print the vehicle type in this std::cout between "THE [" and "] PULL". 
-        std::cout << name << ": YOU IN THE [ " << v << " ] PULL OVER AND SHOW YOUR HANDS" << std::endl;
+		std::string vehicleType = "";
+
+		if(auto* car = dynamic_cast<Car*>(v))
+		{
+			vehicleType = "car";
+		}
+		else if(auto* motorcycle = dynamic_cast<Motorcycle*>(v))
+		{
+			vehicleType = "motorcycle";
+		}
+		else if(auto* semiTruck = dynamic_cast<SemiTruck*>(v))
+		{
+			vehicleType = "truck";
+		}
+        std::cout << name << ": YOU IN THE [ " << vehicleType << " ] PULL OVER AND SHOW YOUR HANDS" << std::endl;
         std::cout << "EVERYONE ELSE, SLOW DOWN!! \n\n\n";
         h->removeVehicle(v);
     }
--- SemiTruck.cpp
+++ SemiTruck.cpp
@@ -4,12 +4,14 @@ SemiTruck::SemiTruck(const std::string& n) : Vehicle(n) {}
 
 void SemiTruck::pullOver()
 {
-
+    setSpeed(0);
+    std::cout << "Hi, officer." << std::endl;
 }
 
 void SemiTruck::laneChange()
 {
-
+    setSpeed(48);
+    std::cout << name << ": blinker " << std::endl;
 }
 
 
